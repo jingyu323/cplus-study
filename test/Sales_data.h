20884fc6300d1535acbabffe6538fc331db8e44f
Rain <xzg323@163.com>
2022-04-17 09:01:50 +0800

    friend

--- test/Sales_data.h
+++ test/Sales_data.h
@@ -3,12 +3,14 @@ using namespace std;
 
 class  Sales_data
 {
-private:
-    string isbn;
-    unsigned units_sold = 0;
-    double renvenus = 0.0;
+friend Sales_data add(const Sa);
 public:
      Sales_data(string isbn);
     
     bool same_isbn(string isbn);
+
+private:
+    string isbn;
+    unsigned units_sold = 0;
+    double renvenus = 0.0;
 };
