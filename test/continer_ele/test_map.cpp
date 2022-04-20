c2dcbee9374ee459ba7dd5158f07ea4a2829bce4
Rain <xzg323@163.com>
2022-04-20 22:32:41 +0800

    map

---
+++ test/continer_ele/test_map.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+
+/**
+ * 关联容器
+ * map
+ * 
+ */
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    map<string , size_t> word_count;
+string word;
+
+while (cin >> word)
+{
+    ++word_count[word];
+}
+
+for (const auto &w :word_count)
+{
+    cout << w.first << " occurs " << w.second << ( (w.second >1 )  ? "times" :"time") << endl;
+}
+
+
+
+    return 0;
+}
