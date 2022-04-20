aeeeba40c68018fc6d1e521cf9337ce2c773b733
Rain <xzg323@163.com>
2022-04-21 06:17:59 +0800

    map find

--- test/continer_ele/test_map.cpp
+++ test/continer_ele/test_map.cpp
@@ -18,15 +18,41 @@ int main(int argc, char const *argv[])
     map<string , size_t> word_count;
 string word;
 
-while (cin >> word)
-{
-    ++word_count[word];
-}
+// while (cin >> word)
+// {
+//     ++word_count[word];
+// }
 
-for (const auto &w :word_count)
-{
-    cout << w.first << " occurs " << w.second << ( (w.second >1 )  ? "times" :"time") << endl;
-}
+// for (const auto &w :word_count)
+// {
+//     cout << w.first << " occurs " << w.second << ( (w.second >1 )  ? "times" :"time") << endl;
+// }
+
+std::map < int , std::string > mapPerson;
+mapPerson.insert(pair < int,string > (1,"Jim"));
+mapPerson.insert(std::map < int, std::string > ::value_type (2, "Tom"));
+
+mapPerson[3] = "jerry";
+
+std::map < int ,std::string > ::iterator it;
+    std::map < int ,std::string > ::iterator itEnd;
+    it = mapPerson.begin();
+    itEnd = mapPerson.end();
+    while (it != itEnd) 
+    {
+		cout<<it->first<<' '<<it->second<<endl;  
+		it++;
+	}
+
+map<int,string > ::iterator l_it;;
+   l_it = mapPerson.find(1);
+   if(l_it != mapPerson.end()){
+                cout<<"we do not find 112"<<endl;
+
+                 std::cout<<"Find, the value is "<<l_it->second<<endl;
+   }
+             
+   else cout<<"wo find 112"<<endl; 
 
 
 
