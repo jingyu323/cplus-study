9d091105e6b29b6261b90118cbf4dab80ccfe61c
Rain <xzg323@163.com>
2023-07-20 23:10:01 +0800

    test c

--- c++first/test_insert.cpp
+++ c++first/test_insert.cpp
@@ -31,14 +31,30 @@ void ListInsert( SqlList &L,int i ,int e ){
 
 }
 
+
+bool ListDelet(SqlList &L,int i ,int &e ){
+
+    if(i < 1 || i > L.length){
+        return false;
+    }
+    e = L.data[i-1];
+
+    for(int j=i;j<L.length;j++){
+        L.data[j-1] = L.data[j];
+    }
+    L.length -- ;
+
+    return true;
+
+
+}
+
 int main(int argc, char const *argv[])
 {
     SqlList L;
 
     InitList(L);
-
-
-
+    
 
     ListInsert(L,3,3);
 
@@ -48,7 +64,14 @@ int main(int argc, char const *argv[])
         printf("data[%d]=%d\n",i,L.data[i]);
     }
 
-
+  int e =3;
+    ListDelet(L,3,e);
+    printf("\n");
+     for (int i = 0; i < L.length; i++)
+    {
+        /* code */
+        printf("data[%d]=%d\n",i,L.data[i]);
+    }
 
     return 0;
 }
