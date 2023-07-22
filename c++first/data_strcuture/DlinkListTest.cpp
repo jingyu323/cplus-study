cb690cdf502e29169b911cefff58a4c2606f6e00
Rain <xzg323@163.com>
2023-07-22 23:10:32 +0800

    dlinklist   print

--- c++first/data_strcuture/DlinkListTest.cpp
+++ c++first/data_strcuture/DlinkListTest.cpp
@@ -13,20 +13,22 @@ bool InitDlinklist(DLinkList &L){
     if(L == NULL){
         return false;
     }
-    L->prior == NULL;
-    L->next == NULL;
+    L->prior = NULL;
+    L->next = NULL;
     return true;
 
 }
 
 bool InsertNextDNode(Dnode *p,Dnode *s){
+ if(p == NULL || s == NULL){ return false ; }
+
     s->next = p->next; //先修改插入节点的指向
     if(p->next != NULL){ // 修改的是后驱节点的前向指针
          p->next->prior = s;
     }
     s->prior = p;  // 修改插入元素的前向指针
     p->next = s;  // 修改p的后驱指针
-
+    return true;
 }
 
 // 删除节点的后继节点
@@ -49,6 +51,47 @@ bool DeleteDNode(Dnode *p){
     return true;
 }
 
+void DestroyList(DLinkList &L){
+    while (L->next != NULL)
+    {
+       DeleteDNode(L);
+    }
+    free(L);
+    L= NULL;
+    
+}
+
+// 前向遍历
+void printAllNodePreRead(DLinkList L){
+    if(L == NULL){
+        return;
+    }
+
+    Dnode *p  = L->prior;
+    while (p->prior != NULL)
+    {
+          printf("%d",p->data);
+          p  = p->prior;
+    }
+
+}
+
+// 后向遍历
+void printAllNodeBackRead(DLinkList L){
+    
+    if(L == NULL){
+        return;
+    }
+
+    Dnode *p =L ;
+
+    while (p->next != NULL)
+    {
+          printf("data is: %d \n",p->next->data);
+          p  = p->next;
+    }
+}
+
 
 
 int main(int argc, char const *argv[])
@@ -56,7 +99,14 @@ int main(int argc, char const *argv[])
     
     DLinkList L;
 
-    InitDlinklist(L);
+    bool inires =   InitDlinklist(L);
+
+    Dnode *s =  (Dnode  *) malloc(sizeof(Dnode));
+    s->data = 9;
+
+    bool res = InsertNextDNode(L,s);
+
+    printAllNodeBackRead(L);
 
 
 
