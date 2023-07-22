a72cbf13dfcd5743a4fd482b59a1a8a1a0880d7d
Rain <xzg323@163.com>
2023-07-22 10:52:18 +0800

    test  stack

--- c++first/data_strcuture/kuohao_stack.cpp
+++ c++first/data_strcuture/kuohao_stack.cpp
@@ -10,19 +10,39 @@ typedef struct
 } SqStatck ;
 
 void InitStatck(SqStatck &S){
-
+    S.top = -1;
 }
 
 
 bool StackEmpty(SqStatck s){
+    if(s.top == -1){
+        return true;
+    }else{
+        return false;
+    }
 
 }
 
 bool Push(SqStatck &S, char x){
 
+// 栈满了
+    if(S.top == MaxSize -1){
+        return false;
+    }
+    S.top = S.top +1;
+
+    S.data[S.top] = x;
+    return true;
+
 }
 
-bool Pop(SqStatck s, char &x){
+bool Pop(SqStatck  &s, char &x){
+    if(s.top  == -1 ){
+        return  false;
+    }
+    x  = s.data[s.top];
+    s.top = s.top -1;
+    return true;
 
 }
 
@@ -34,7 +54,7 @@ bool bracketCheck(char str[], int length){
     for (int  i = 0; i < length; i++)
     {
         if(str[i] == '('  || str[i] == '{'  || str[i] == '[' ){
-            Push(S,str[i])；
+            Push(S,str[i]);
         }else{
             if(StackEmpty(S)){
                 return false;
@@ -56,20 +76,26 @@ bool bracketCheck(char str[], int length){
                 return false;
             } 
 
-
         }
-        
-
     }
     return StackEmpty(S);
-
 }
 
 int main(int argc, char const *argv[])
 {
     
+    char str1[] = {'{','}','(',')'};
 
+    bool res =  bracketCheck(str1,4);
+    printf("%d\n",res);
 
-    
+bool b = true;
+
+printf("%d\n",b);
+
+bool c = false;
+
+printf("%d\n",c);
+
     return 0;
 }
