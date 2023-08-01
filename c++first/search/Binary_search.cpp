827149baf3568c8afb97ac5bf4dc787585a2c582
Rain <xzg323@163.com>
2023-08-01 22:16:02 +0800

    grph

---
+++ c++first/search/Binary_search.cpp
@@ -0,0 +1,39 @@
+
+
+//  折半查找
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct 
+{
+     int *elem;
+     int tableLen;
+} SSTable;
+
+
+
+int Binary_search(SSTable L , int key){
+
+    int low =0,hight=L.tableLen-1,mid;
+    while (low <= hight)
+    {
+        mid = (low + hight) /2;
+
+        if(L.elem[mid] == key){
+            return mid;
+
+        }else if (L.elem[mid] > key)
+        {
+            hight = mid -1;
+        }else{
+            low = mid +1;
+        }
+        
+    }
+    
+
+
+    return 0;
+
+}
