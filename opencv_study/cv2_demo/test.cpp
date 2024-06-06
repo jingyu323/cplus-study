e392680bf2e36a25480beb06b6946432f398dfd8
jingyu323 <xzg323@163.com>
2024-06-06 11:21:43 +0800

    cv2

---
+++ opencv_study/cv2_demo/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    Mat srcImage = imread("card2.png");
+    if (!srcImage.data) {
+        std::cout << "Image not loaded";
+        return -1;
+    }
+    imshow("image", srcImage);
+    waitKey(0); 
+    return 0;
+}
