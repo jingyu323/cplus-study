d6505d7fab2351efcb37a1ec0c9fa32d769ffa4c
jingyu323 <xzg323@163.com>
2024-07-08 17:53:18 +0800

    C++

--- opencv_study/cv2_demo/test.cpp
+++ opencv_study/cv2_demo/test.cpp
@@ -1,16 +1,19 @@
+#pragma comment(lib, "opencv_world490.lib")
 #include <iostream>
 #include "opencv2/opencv.hpp"
 
-using namespace std;
+using namespace std; 
 using namespace cv;
 
 int main() {
     Mat srcImage = imread("card2.png");
-    if (!srcImage.data) {
-        std::cout << "Image not loaded";
-        return -1;
-    }
-    imshow("image", srcImage);
-    waitKey(0); 
+    // // if (!srcImage.data) {
+    // //     std::cout << "Image not loaded";
+    // //     return -1;
+    // // }
+    // // imshow("image", srcImage);
+    // waitKey(0); 
+
+    cout << "Hello World"; 
     return 0;
 }
