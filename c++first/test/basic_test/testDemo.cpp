16b89b68bdc784c1d23ccf5f07a104a61e78d8ef
jingyu323 <xzg323@163.com>
2024-07-12 13:57:39 +0800

    deque

--- c++first/test/basic_test/testDemo.cpp
+++ c++first/test/basic_test/testDemo.cpp
@@ -77,4 +77,30 @@ int main()
         ++first;
     }
     
+    printf("\n===================rbegin ==============\n");
+     for (auto i = d.rbegin(); i < d.rend(); i++) {
+        cout << *i << " ";
+    }
+
+    printf("\n===================insert ==============\n");
+     //第一种格式用法
+    d.insert(d.begin() + 1, 3);//{1,3,2}
+    //第二种格式用法
+    d.insert(d.end(), 2, 5);//{1,3,2,5,5}
+    //第三种格式用法
+   
+    d.insert(d.end(), test.begin(), test.end());//{1,3,2,5,5,7,8,9}
+    //第四种格式用法
+    d.insert(d.end(), { 10,11 });//{1,3,2,5,5,7,8,9,10,11}
+    //调用push_front()向容器头部添加数据。
+    d.push_front(2);//{2}
+    //调用pop_front()移除容器头部的一个数据。
+    d.pop_front();//{}
+    //调用 emplace 系列函数，向容器中直接生成数据。
+    d.emplace_back(2); //{2}
+    d.emplace_front(3); //{3,2}
+    for (int i = 0; i < d.size(); i++) {
+        cout << d[i] << " ";
+    }
+
 }
