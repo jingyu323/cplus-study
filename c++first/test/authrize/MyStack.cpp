cf49e3d9843fe1fae50659b48a04ee337bc91076
jingyu323 <xzg323@163.com>
2024-07-25 19:35:35 +0800

    kres

--- c++first/test/authrize/MyStack.cpp
+++ c++first/test/authrize/MyStack.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
-using namespace std;
+#include <queue>
+#include <stack>
+using namespace std; 
 class MyStack
 {
-private:
+public:
     queue<int> data;
     /* data */
 public:
     MyStack(/* args */);
     ~MyStack();
 
-    void push(int x){
-         queue<int> tmp_que;
-
+    void push(int x);
+    
+    int pop(){
+        int x = data.front();
+        data.pop();
+        return x;
+    }
+    int top(){
+        return data.front();
+    }
+    bool isEmpty(){
+        return data.empty();
     }
 };
 
@@ -23,9 +34,34 @@ MyStack::~MyStack()
 {
 }
 
+void MyStack::push(int x){
+       std::queue<int> tmp_que;
+         tmp_que.push(x);
+         while (!data.empty())
+         {
+            tmp_que.push(tmp_que.front());
+
+            data.pop();
+         }
+
+}
+
 
 int main(int argc, char const *argv[])
 {
     /* code */
+     std::queue<int> tmp_que;
+     stack<int> S1;
+     
+     S1.push(12);
+     
+     int a = S1.pop();
+ 
+
+     cout<< " head3: "<< a <<endl;
+
+    
+
+
     return 0;
 }
