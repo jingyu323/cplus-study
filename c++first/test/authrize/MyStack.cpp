983c223376ebf5c07010cdda0cdd40a07c319942
jingyu323 <xzg323@163.com>
2024-07-25 18:47:29 +0800

    c++ node

---
+++ c++first/test/authrize/MyStack.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class MyStack
+{
+private:
+    queue<int> data;
+    /* data */
+public:
+    MyStack(/* args */);
+    ~MyStack();
+
+    void push(int x){
+         queue<int> tmp_que;
+
+    }
+};
+
+MyStack::MyStack(/* args */)
+{
+}
+
+MyStack::~MyStack()
+{
+}
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
