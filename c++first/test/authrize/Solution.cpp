30aa31a8676aa8b5ebb8e1400eaad63203dff517
jingyu323 <xzg323@163.com>
2024-07-26 09:14:03 +0800

    kres

---
+++ c++first/test/authrize/Solution.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+
+
+class Solution
+{
+private:
+    /* data */
+public:
+    Solution(/* args */);
+    ~Solution();
+    int minSubArrayLen(int target, vector<int>& nums){
+        
+        return 0;
+        
+    }
+};
+
+Solution::Solution(/* args */)
+{
+}
+
+Solution::~Solution()
+{
+}
