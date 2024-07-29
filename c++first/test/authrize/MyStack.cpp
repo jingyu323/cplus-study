0a9efbae81ebc61f81216e33672c9fd5196b53e4
jingyu323 <xzg323@163.com>
2024-07-29 16:14:55 +0800

    动态规划

--- c++first/test/authrize/MyStack.cpp
+++ c++first/test/authrize/MyStack.cpp
@@ -385,6 +385,30 @@ public:
         return dp[n];
 
     }
+    int tribonacci(int n) {
+
+         if (n == 0) {
+            return 0;
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        int p = 0, q = 0, r = 1, s = 1;
+        for (int i = 3; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = s;
+            s = p + q + r;
+        }
+        return s;  
+
+    }
+
+    int minCostClimbingStairs(vector<int>& cost) {
+
+        
+
+    }
 
 };
 
