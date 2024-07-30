13423617c8e0d57987308bf7a8e0fc84ac07f17e
jingyu323 <xzg323@163.com>
2024-07-30 16:40:27 +0800

    动态规划

--- c++first/test/authrize/MyStack.cpp
+++ c++first/test/authrize/MyStack.cpp
@@ -461,6 +461,66 @@ public:
 
     }
 
+    int uniquePaths(int m, int n) {
+        // int dp[m][n]={0};
+
+        vector<vector<int>> dp (m, vector<int>(n));
+
+
+        for (int i = 0; i < m; ++i) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; ++j) {
+           dp[0][j] = 1;
+        }
+ 
+
+        for ( int i = 1; i < m; i++)
+        {
+            
+            for (int j = 1; j < n; j++)
+            {
+                  
+                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+            
+        }
+         return dp[m - 1][n - 1];
+    }
+
+        int minPathSum(vector<vector<int>>& grid) {
+
+            int m =  grid.size();
+            int n = grid[0].size();
+
+            vector<vector<int>> dp (m, vector<int>(n,0));
+
+             dp[0][0] = grid[0][0];
+
+            for (int i = 1; i < m; ++i) {
+            dp[i][0] = grid[i][0] +dp[i-1][0];
+            }
+            for (int j = 1; j < n; ++j) {
+            dp[0][j] = grid[0][j] +dp[0][j-1];
+            }
+
+    
+
+            for ( int i = 1; i < m; i++)
+            {
+                
+                for (int j = 1; j < n; j++)
+                {
+                    
+                    dp[i][j] = min(dp[i - 1][j],dp[i][j - 1])  + grid[i][j] ;
+                     cout << "=======sum : " << dp[i][j] <<  endl; 
+                }
+                
+            }
+            return dp[m - 1][n - 1];
+
+        }
+
 };
 
 MyStack::MyStack(/* args */)
