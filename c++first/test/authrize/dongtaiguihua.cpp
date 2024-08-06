7e7ef539651a04e61d00b16f27b55d994c489e35
jingyu323 <xzg323@163.com>
2024-08-06 19:52:17 +0800

    动态规划

--- c++first/test/authrize/dongtaiguihua.cpp
+++ c++first/test/authrize/dongtaiguihua.cpp
@@ -124,6 +124,24 @@ int minPathSum(vector<vector<int>>& grid) {
 
     }
 
+    int integerBreak(int n) {
+
+        vector<int> dp(n+1);
+        dp[1] = 1;
+        for (int i = 1; i <= n; i++)
+        {   
+            for (int j = 1; j < i-1; j++)
+            {
+                dp[i] = max(dp[i],  max(j * dp[i - j], j * (i - j)));
+            }
+            
+            
+            
+        }
+        return dp[n];
+
+    }
+
 
 };
 
